d3ea2da7b217d1192512b6702bdcd08d252f7ddf
Eng-MedhatLaz <159828358+Eng-MedhatLaz@users.noreply.github.com>
2024-11-17 19:52:41 +0200

    Done

---
+++ README.md
@@ -0,0 +1,1 @@
+# Initcard-project
---
+++ js/main.js
@@ -0,0 +1,42 @@
+// document.addEventListener('scroll', function() {
+//     const circle = document.querySelector('.circle');
+//     const scrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight);
+//     const dashoffset = 150.796 * (1 - scrollPercentage);
+//     circle.style.strokeDashoffset = dashoffset;
+// });
+ 
+
+$(document).ready(function() {      
+
+    // $(document).mousemove(function(e) {
+    //     $('#dot').css({
+    //     left: (e.clientX) + 'px',
+    //     top: (e.clientY) + 'px'
+    //     });
+    // });
+    // function toBigSmallNav() { 
+    //     if ($('body, html').outerWidth() < 1000) {
+            
+    //         $(".menu-extras").fadeIn(); // إظهار قائمة "menu-extras" بسلاسة
+    //         $("#navmenu-nav").fadeOut(); // إخفاء قائمة "navmenu-nav" بسلاسة
+    //         $("ul.appended-menu").fadeIn(); // إظهار القائمة المضافة بسلاسة
+            
+    //         // console.log("small nav");
+    //     } else {
+            
+    //         $(".menu-extras").fadeOut(); // إخفاء قائمة "menu-extras" بسلاسة
+    //         $("ul.appended-menu").fadeOut(); // إخفاء القائمة المضافة بسلاسة
+    //         $("#navmenu-nav").fadeIn(); // إظهار قائمة "navmenu-nav" بسلاسة
+            
+    //         // console.log("big nav");
+    //     }
+    //     // console.log("body width", $('body, html').outerWidth());
+    // }
+    // $(window).resize( function() {
+    //     toBigSmallNav();
+    // }    );
+    // toBigSmallNav();
+    
+    console.log("project done");
+        
+});
